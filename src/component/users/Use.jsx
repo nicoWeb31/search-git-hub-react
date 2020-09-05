d7a2bf412b46ee70a4f41cf03dfe6ca879428166
nicoWeb31 <nico.riot@free.fr>
2020-09-05 10:18:02 +0200

    user infos

--- src/component/users/Use.jsx
+++ src/component/users/Use.jsx
@@ -14,7 +14,7 @@ const Use = ({match, user,loading,getUser}) => {
     }, [])
 
 
-    const {name, avatar_url,location,bio,blog,login,html_url,public_repos,public_gists,hireable} = user;
+    const {name, avatar_url,location,bio,blog,login,html_url,public_repos,public_gists,hireable,company,followers,following} = user;
 
 
     if(loading)return <Spinner/>;
@@ -33,6 +33,55 @@ const Use = ({match, user,loading,getUser}) => {
             <div className="all-center">
                 <img src={avatar_url} alt="avatar...." className="round-img" style={styleAvart}/>
                 <h1>{name}</h1>
+                <p>{location}</p>
+            </div>
+        <div>
+            {bio && (
+            <>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+            </>
+            )
+            }
+            <a href={html_url} className="btn btn-dark my-1">Visit Github profile</a>
+            <ul>
+                <li>
+                    {login && (
+                        <>
+                        <strong>Username :</strong> {login}
+                        </>
+                    )}
+                </li>
+                <li>
+                    {company && (
+                        <>
+                        <strong>Company :</strong> {company}
+                        </>
+                    )}
+                </li>
+                <li>
+                    {blog && (
+                        <>
+                        <strong>Website :</strong> {blog}
+                        </>
+                    )}
+                </li>
+            </ul>
+
+        </div>
+        </div>
+        <div className="card text-center">
+            <div className="badge badge-primary">
+                Followers : {followers}
+            </div>
+            <div className="badge badge-success">
+                Following : {following}
+            </div>
+            <div className="badge badge-light">
+                Public Respos : {public_repos}
+            </div>
+            <div className="badge badge-dark">
+                Public Gists : {public_gists}
             </div>
         </div>
 
