15b97cf503ecbdb49371121b0c59afebe53586eb
nicoWeb31 <nico.riot@free.fr>
2020-09-05 10:01:36 +0200

    card git

--- src/component/users/Use.jsx
+++ src/component/users/Use.jsx
@@ -1,21 +1,51 @@
 import React, { useEffect } from 'react';
+import Spinner from "../spinner/Spinner";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom"
 
-const Use = (props) => {
+const Use = ({match, user,loading,getUser}) => {
+
+    const styleAvart ={
+        width: '150px'
+    }
 
     useEffect(() => {
-        props.getUser(props.match.params.login)
+        getUser(match.params.login)
     }, [])
 
 
-    const {name, avatar_url,location,bio,blog,login,html_url,public_repos,public_gists,hireable} = props.user
+    const {name, avatar_url,location,bio,blog,login,html_url,public_repos,public_gists,hireable} = user;
+
+
+    if(loading)return <Spinner/>;
+
     return (
 
-        <div>
-        <h1>{name}</h1>
+        
+
+        <>
+        <Link to="/" className="btn btn-light">Back to search</Link>
+        Hireable: {''}
+        {
+            hireable ? <i className='fas fa-check text-success'/> : <i className='fas fa-times-circle text-danger'/>
+        }
+        <div className="card grid-2">
+            <div className="all-center">
+                <img src={avatar_url} alt="avatar...." className="round-img" style={styleAvart}/>
+                <h1>{name}</h1>
+            </div>
         </div>
 
 
+        </>
+
+
     );
+}
+
+Use.propTypes = {
+    user: PropTypes.object.isRequired,
+    loading: PropTypes.bool.isRequired
 }
 
 export default Use;
